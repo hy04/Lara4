4baa610970db5b5aa96dffbe25a8f977670b7542
He Yang <heyang@Hes-MacBook-Pro.local>
2019-02-04 16:05:42 +1300

    img uploader done

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -9,8 +9,25 @@ class AlbumsController extends Controller
     public function index(){
       return view('albums.index');
     }
-
     public function create(){
       return view('albums.create');
+    }
+    public function store(Request $request){
+      $this->validate($request, [
+        'name' => 'required',
+        'cover_image' => 'image|max:1999'
+      ]);
+      //GET file name with extension
+      $filenameWithExt= $request->file('cover_image')->getClientOriginalName();
+      //get file name
+      $filename=pathinfo($filenameWithExt, PATHINFO_FILENAME);
+      //get extension
+      $extension=$request->file('cover_image')->getClientOriginalExtension();
+      //create new filename with timestamp
+      $filenameToStore = $filename.'_'.time().'.'.$extension;
+
+      //upload image
+      $path=$request->file('cover_image')->storeAs('public/album_covers', $filenameToStore);
+      return $path;
     }
 }
