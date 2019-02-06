e7c8b9e244fb03692872dbfc39553e4c5d704160
He Yang <heyang@Hes-MacBook-Pro.local>
2019-02-06 16:14:27 +1300

    create album and upload photos

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -1,6 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-  <h3>Albums</h3>
+<!-- check if there are any albums at all -->
+  @if(count($albums) > 0)
+    <?php
+      $colcount = count($albums);//column count
+  	  $i = 1;
+    ?>
+    <div id="albums">
+      <div class="row text-center">
+        <!-- display the albums -->
+        @foreach($albums as $album)
+          @if($i == $colcount)
+             <div class='medium-4 columns end'>
+               <a href="/albums/{{$album->id}}">
+                  <img class="thumbnail" src="storage/album_covers/{{$album->cover_image}}" alt="{{$album->name}}">
+                </a>
+               <br>
+               <h4>{{$album->name}}</h4>
+          @else
+            <div class='medium-4 columns'>
+              <a href="/albums/{{$album->id}}">
+                <img class="thumbnail" src="storage/album_covers/{{$album->cover_image}}" alt="{{$album->name}}">
+              </a>
+              <br>
+              <h4>{{$album->name}}</h4>
+          @endif
+        	@if($i % 3 == 0)
+          </div></div><div class="row text-center">
+        	@else
+            </div>
+          @endif
+        	<?php $i++; ?>
+        @endforeach
+      </div>
+    </div>
+  @else
+    <p>No Albums To Display</p>
+  @endif
 
 @endsection
